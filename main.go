99c5bf440c77d206fd273a520e046f3fafc234c1
Vincent Demeester <vincent@sbr.pm>
2015-05-12 22:26:42 +0200

    Add codegansta/cli and a --bind option

--- main.go
+++ main.go
@@ -3,11 +3,35 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/codegangsta/cli"
 )
 
 func main() {
 
-	router := NewRouter()
+	app := cli.NewApp()
+	app.Name = "miniatures-todo"
+	app.Usage = "Run a TODOs webservice (back in memory)"
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "bind, b",
+			Value:  "127.0.0.1:8080",
+			Usage:  "host string to bind on",
+			EnvVar: "MIMIATURE_TODOS_HOST",
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		host := "127.0.0.1:8080"
+		if c.IsSet("bind") {
+			host = c.String("bind")
+		}
+		router := NewRouter()
+		log.Printf("Serving HTTP on %s", host)
+		log.Fatal(http.ListenAndServe(host, router))
+	}
+
+	app.Run(os.Args)
 }
